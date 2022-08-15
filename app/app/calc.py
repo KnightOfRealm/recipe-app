88fa98886f7f75d4f0792d8a395a6a47187a21c5
Abhishek Singh <abhishek.singh@hardkskills.com>
2022-08-15 08:38:40 +0200

    added postgres

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculate functions
+"""
+
+def add(x,y):
+    """ Add x and y and return result."""
+    return x + y
+
+def substract(x,y):
+    """ subtract x from y and return result."""
+    return y - x
